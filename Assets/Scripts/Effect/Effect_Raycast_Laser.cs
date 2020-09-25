fde30e9f798e91223899f318152a77bd8e5d409c
Hanhyeok <hwanghhh82@gmail.com>
2020-09-25 15:46:19 +0900

    Add Attack Method : Lazer

---
+++ Assets/Scripts/Effect/Effect_Raycast_Laser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_Raycast_Laser : MonoBehaviour {
+    
+    //2#
+    public GameObject Raybody; //레이캐스팅을 쏘는 위치
+    public GameObject ScaleDistance; //거리에 따른 스케일 변화를 위한 오브젝트 대상
+    public GameObject RayResult; //충돌하는 위치에 출력할 결과
+    
+    // Use this for initialization
+    void Start () {
+    
+    }
+	
+    // Update is called once per frame
+    void Update () {
+
+        //레이캐스팅 결과정보를 hit라는 이름으로 정한다.
+        RaycastHit hit; 
+
+        //레이캐스트 쏘는 위치, 방향, 결과값, 최대인식거리
+        Physics.Raycast(transform.position, transform.forward, out hit, 200);
+
+        //거리에 따른 레이저 스케일 변화
+        ScaleDistance.transform.localScale = new Vector3(1, hit.distance, 1);
+
+        //레이캐스트가 닿은 곳에 오브젝트를 옮긴다.
+        RayResult.transform.position = hit.point;
+
+        //해당하는 오브젝트의 회전값을 닿은 면적의 노멀방향와 일치시킨다.
+        RayResult.transform.rotation = Quaternion.LookRotation(hit.normal); 
+    }
+}
---
+++ Assets/Scripts/Lazercast.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazercast : MonoBehaviour
+{
+    public GameObject Raybody; //레이캐스팅을 쏘는 위치
+    public GameObject ScaleDistance; //거리에 따른 스케일 변화를 위한 오브젝트 대상
+    //public GameObject RayResult; //충돌하는 위치에 출력할 결과
+    // Start is called before the first frame update
+    public LayerMask isLayer;
+    void Start()
+    {
+        Invoke("DestroyBullet",0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.right, new Color(0, 1, 0));
+
+        //레이캐스트 쏘는 위치, 방향, 결과값, 최대인식거리
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 20, isLayer);
+
+        //거리에 따른 레이저 스케일 변화
+        if(hit.collider != null){
+            ScaleDistance.transform.localScale = new Vector2(hit.distance/2, 1);
+            if(hit.collider.tag == "Enemy"){
+                OnAttack(hit.collider.transform);
+                Invoke("DestroyBullet", 0.1f);
+            }
+        }
+        else{
+            ScaleDistance.transform.localScale = new Vector2(20, 1);
+        }
+
+    }
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+    void OnAttack(Transform enemy)
+    {
+        EnemyMove enemyMove = enemy.GetComponent<EnemyMove>();
+        enemyMove.OnDamaged();
+    }
+}
