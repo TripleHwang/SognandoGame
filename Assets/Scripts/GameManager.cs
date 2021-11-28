aecad17418fbf50a50591064c61fe9f113ad8d78
Persona-L <dlaq310@naver.com>
2021-11-28 15:37:35 +0900

    11월 28일 업데이트
    메인메뉴에 불러오기 버튼 추가
    불러오기 기능 추가
    사망 화면 추가
    사망 후 재시도 버튼 누를 시 불러오기 기능 활성화

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,47 @@ public class GameManager : MonoBehaviour
     public int stagePoint;
     public int stageIndex;
     public int health;
-    public PlayerMove player;
+    public PlayerMove playerMove;
     public GameObject[] stages;
+    public GameObject menuSet;
+    public GameObject player;
+    public GameObject youDied;
+    
     // Start is called before the first frame update
+    void Start()
+    {
+        GameLoad(); // 게임 시작하면 세이브부터 로드
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) // ESC를 누르면
+            menuSet.SetActive(!menuSet.activeSelf); // 메뉴창 활성화/비활성화
+    }
+
+    public void GameSave() // 세이브 로직
+    {
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x); // 플레이어 캐릭터의 X 좌표값 저장
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y); // 플레이어 캐릭터의 Y 좌표값 저장
+        PlayerPrefs.Save(); // 저장 함수
+        menuSet.SetActive(false); // 저장하면 메뉴창 비활성화
+    }
+
+    public void GameLoad() // 불러오기 로직
+    {
+        menuSet.SetActive(false);
+        if (!PlayerPrefs.HasKey("PlayerX"))
+            return;
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        player.transform.position = new Vector3(x, y, 0);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
     public void NextStage()
     {
         stages[stageIndex].SetActive(false);
@@ -28,9 +66,19 @@ public class GameManager : MonoBehaviour
             health--;
         else
         {
-            player.OnDie();
+            playerMove.OnDie();
             Debug.Log("You Died!");
+            youDied.SetActive(!youDied.activeSelf);
         }
+    }
+
+    public void Retry()
+    {
+        health++;
+        playerMove.OffDie();
+        youDied.SetActive(!youDied.activeSelf);
+        menuSet.SetActive(false);
+        GameLoad();
     }
 
     // Update is called once per frame
@@ -47,6 +95,6 @@ public class GameManager : MonoBehaviour
 
     void PlayerReposition(){
         player.transform.position = new Vector3(0,0,-1);
-        player.VelocityZero();
+        playerMove.VelocityZero();
     }
 }
